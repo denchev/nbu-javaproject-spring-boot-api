f7af26e7a00d2a0c793dd05507743860caec6d16
Marush Denchev <avreon@gmail.com>
2021-08-10 09:03:40 +0300

    Do not allow overbooking

--- src/main/java/com/nbu/javaproject/doctors/appointment/AppointmentRepository.java
+++ src/main/java/com/nbu/javaproject/doctors/appointment/AppointmentRepository.java
@@ -1,6 +1,9 @@
 package com.nbu.javaproject.doctors.appointment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    @Query(value = "SELECT * FROM appointments WHERE doctor_id = ?1 AND patient_id = ?2", nativeQuery = true)
+    Appointment findByDoctorIdAndPatientId(Long doctorId, Long patientId);
 }
--- src/main/java/com/nbu/javaproject/doctors/appointment/AppointmentService.java
+++ src/main/java/com/nbu/javaproject/doctors/appointment/AppointmentService.java
@@ -11,6 +11,12 @@ public class AppointmentService {
     }
 
     public void save(Appointment appointment) {
+        // Check if there is similar appointment already
+        Appointment similarAppointment = this.appointmentRepository.findByDoctorIdAndPatientId(appointment.getDoctor().getId(), appointment.getPatient().getId());
+
+        if (similarAppointment != null) {
+            throw new IllegalStateException("There is an appointment set for this doctor and this patient");
+        }
         this.appointmentRepository.save(appointment);
     }
 }
